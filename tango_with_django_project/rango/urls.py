ab01a5122e8e0c50c96e0571c51b02fc9a99e4e8
“Faraj <2736261a@student.gla.ac.uk>
2025-02-13 15:02:44 +0000

    7

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,9 +1,16 @@
+from django.contrib import admin
 from django.urls import path
-
+from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+    path('add_category/', views.add_category, name='add_category'),
 ]
